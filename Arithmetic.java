9fd75f28aa88b48407252f533fb4a8e1c2cdcd02
Sourabh Raje <sourabh.raje@kiprosh.com>
2023-09-12 12:38:28 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,15 @@
+package Arithmetic;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a=14, b=5;
+		float c = (float)a/b;
+		int r = a%b;
+		
+		System.out.println(c);
+		System.out.println(r);
+
+	}
+
+}

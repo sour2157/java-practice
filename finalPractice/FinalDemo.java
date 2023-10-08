a9a9d6ab0e18e0e14782986472d4268e3e627487
Sourabh Raje <sour2157@gmail.com>
2023-10-08 17:09:02 +0530

    Add final keyword practice programs

---
+++ finalPractice/FinalDemo.java
@@ -0,0 +1,21 @@
+package finalPractice;
+
+public class FinalDemo {
+	
+	final float PI = 3.14523f;
+	final int TEST;
+	final int TENT;
+	
+	{
+		TEST=123;
+	}
+	
+	public FinalDemo() {
+		TENT=456;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
